c6078a00c6d7cf06c5cc9cda212b5b594eb9c485
lores42 <148881509+lores42@users.noreply.github.com>
2024-09-25 15:59:18 +0200

    exrcisse

---
+++ 01-Algorithmes/Algorithmes/EsTuMajeur/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace EsTuMajeur_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Es tu majeur ?");
+            // Variable
+
+            int numA;
+
+
+            string saisieUtilisateur;
+
+            // traitement 
+
+            Console.WriteLine("entrer votre age");
+            saisieUtilisateur = Console.ReadLine(); // Récupération d'une saisie utilisateur
+            numA = int.Parse(saisieUtilisateur); // Conversion de la saisie en nombre entier
+
+
+            //Affichage
+
+            if (numA <= 0)
+            {
+                Console.WriteLine("Vous n'ets pas encore né!!!");
+            }
+
+            else if (numA >= 0 && numA < 18)
+            {
+                Console.WriteLine("Vous êtes mineur !!!");
+            }
+            else //if (numA >= 18)
+            {
+                Console.WriteLine("Vous êtes majeur !!!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 01-Algorithmes/Algorithmes/MaRetraite/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace MaRetraite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ma retraite");
+
+            //variable
+
+            int numA;
+
+            int[] nombre;
+            string saisieUtilisateur;
+
+            // Traitement 
+
+            Console.WriteLine("Écrivez votre age !!!");
+            saisieUtilisateur = Console.ReadLine(); // Récupération d'une saisie utilisateur
+            numA = int.Parse(saisieUtilisateur); // Conversion de la saisie en nombre entier
+
+            // Affichage
+            if (numA < 0)
+  
+            {
+                Console.WriteLine("La valeur fournie n'est pas un age valide !!!");
+            }
+
+            else if (numA == 60)
+            {
+                Console.WriteLine("C'est le moment de prendre sa retraite !");
+            }
+
+            else if (numA < 60)
+            {
+                Console.WriteLine("Il vous reste " + (60 - numA) + " d'année(s) avant la retraite !");
+            }
+
+            else if (numA > 60)
+            {
+                Console.WriteLine("Vous êtes à la retraite depuis " + (numA - 60) + " d'année(s)!");
+            }
+            
+        }
+        
+
+    }
+}
