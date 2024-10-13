4f2b50cfb66a8ba5e8549425063b0e8d4a567375
lores42 <148881509+lores42@users.noreply.github.com>
2024-10-13 20:43:02 +0200

    Fin Travail

---
+++ 01-Algorithmes/Algorithmes/controlerLaSaisieAvecLimite/controlerLaSaisieAvecLimite/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Controler la saisie avec limite!");
+
+//Variable
+
+string mdp;
+string mdpValide = "formation";
+int i = 0;
+
+//Traitement
+
+Console.WriteLine("saisissez votre mots de passe");
+mdp=Console.ReadLine();
+if (mdp == mdpValide)
+{
+    Console.WriteLine("vous êtes connectez!!!");
+}
+
+else
+{
+   
+    Console.WriteLine(" votre comte est bloquer !!!");
+}
+
+while (i <= 3)
+{
+    if (mdp == mdpValide || i <= 3)
+    
+        {
+            Console.WriteLine("veuillez resaisir votre mots de passe");
+            mdp = Console.ReadLine();
+            //Console.WriteLine(" Votre compte est bmoqué!!!");
+        }
+
+    else 
+        {
+            Console.WriteLine("votre compte est bloqué!!!");
+        break;
+        }
+}
