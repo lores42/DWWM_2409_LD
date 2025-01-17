4d53346d9044a05d735c2a151534ada781f0bc0e
lores42 <148881509+lores42@users.noreply.github.com>
2025-01-17 11:57:16 +0100

    employer_suite

--- 02-WEBFRONT/base_de_donnee_SQL/requette_departement.sql
+++ 02-WEBFRONT/base_de_donnee_SQL/requette_departement.sql
@@ -63,6 +63,84 @@ WHERE deptno = 20;
 
 -- 13. Donner le salaire le plus élevé par département
 SELECT departements.dname, MAX(sal),  employes.deptno
-FROM employes natural join departements   GROUP BY employes.deptno;
+FROM employes 
+natural join departements   
+GROUP BY employes.deptno;
+
+-- 14. Donner département par département masse salariale, nombre d'employés, salaire moyen par type d'emploi.
+SELECT deptno, dname,job, sum(sal+ifnull(comm, 0)) as "masse salariale", round(avg(sal),2) as "salaire moyen", count(empno) as "effectif"
+FROM employes
+NATURAL JOIN departements
+GROUP BY employes.deptno,job;
+
+-- 15. Même question mais on se limite aux sous-ensembles d'au moins 2 employés
+SELECT deptno, dname,job, sum(sal+ifnull(comm, 0)) as "masse salariale", round(avg(sal),2) as "salaire moyen", count(empno) as "effectif"
+FROM employes
+NATURAL JOIN departements
+GROUP BY employes.deptno,job    having count(empno) >=2 ;
+
+-- 16. Liste des employés (Nom, département, salaire) de même emploi que JONES
+select ename, deptno, sal
+FROM employes	
+WHERE job = (SELECT job 
+from employes
+where ename = "jones") and ename != "jones";
+
+-- 17. Liste des employés (nom, salaire) dont le salaire est supérieur à la moyenne globale des salaires
+   select ename , sal,  round((SELECT avg(sal)  FROM employes),2) as " salaire moyen" from employes where sal> (SELECT avg(sal)  FROM employes);
+
+CREATE TABLE projet (
+num_proj SMALLINT AUTO_INCREMENT,
+nom_proj CHAR(5) NOT NULL,
+budget_proj DECIMAL(8,2) NOT NULL, CONSTRAINT PK_projet PRIMARY KEY(num_proj)
+);
+
+ ALTER TABLE projet AUTO_INCREMENT = 101;
+
+INSERT INTO projet 
+(nom_proj, budget_proj)
+VALUES
+('alpha',96000),
+('beta',82000),
+('gamma',15000);
 
 
+
+ALTER TABLE employes ADD  num_proj SMALLINT;
+
+UPDATE employes SET num_proj = 101
+WHERE deptno = 30 AND job = 'salesman';
+UPDATE employes SET num_proj= 102
+WHERE deptno = 30 and job != "salesman" XOR deptno != 30;
+ALTER TABLE employes add CONSTRAINT fk_proj FOREIGN KEY(num_proj) REFERENCES projet(num_proj);
+
+
+-- 20. Créer une vue comportant tous les employés avec nom, job, nom de département et nom de projet
+create view employes_proj
+as SELECT employes.ename, employes.job, departements.dname, projet.nom_proj
+FROM departements 
+NATURAL JOIN employes 
+NATURAL JOIN projet;
+
+
+-- deuxième partie
+
+-- 1. Afficher la liste des managers des départements 20 et 30
+SELECT ename, job
+FROM employes
+WHERE deptno IN(20, 30) and job ="manager";
+
+-- 2. Afficher la liste des employés qui ne sont pas manager et qui ont été embauchés en 81
+SELECT ename, job, hiredate
+FROM employes
+where job != 'manager' and hiredate like '1981%';
+
+-- 3. Afficher la liste des employés ayant une commission
+SELECT ename, job, comm
+FROM employes
+WHERE comm;
+
+-- 4. Afficher la liste des noms, numéros de département, jobs et date d'embauche triés par Numero de Département et JOB les derniers embauches d'abord.
+SELECT ename, deptno, job, hiredate
+FROM employes
+ORDER BY hiredate DESC, deptno , job ;
