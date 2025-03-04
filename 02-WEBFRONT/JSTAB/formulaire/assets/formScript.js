359797658c74f1124c5c31d5603300258da4737f
lores42 <148881509+lores42@users.noreply.github.com>
2025-03-04 16:58:07 +0100

    manipulerTableau

---
+++ 02-WEBFRONT/JSTAB/formulaire/assets/formScript.js
@@ -0,0 +1,8 @@
+const age = document.querySelector("#age")
+const prenom = document.querySelector("#prenom")
+const valide = document.querySelector(".valide")
+const vide = document.querySelector("vide")
+const resultat = document.querySelector(".resultat")
+
+
+resultat.innerText = prenom.value + " " + age.value
