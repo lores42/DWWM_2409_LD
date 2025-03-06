4fb79882cb5c4a61cf1200a0a0d959b442682a20
lores42 <148881509+lores42@users.noreply.github.com>
2025-03-06 16:57:03 +0100

    manipulationTableau2

--- 02-WEBFRONT/JSTAB/manipulerTableau/assets/script.js
+++ 02-WEBFRONT/JSTAB/manipulerTableau/assets/script.js
@@ -1,47 +1,72 @@
-const people = ['Mike Dev', 'John Makenzie', 'Léa Grande']
+const people = ['Mike Dev', 'John Makenzie', 'Byrôn Méssia']
 
 const form = document.querySelector("form")
 const inputFirstname = document.querySelector("#input-firstname")
 const inputLastname = document.querySelector("#input-lastname")
 const btnAdd = document.querySelector("#btn-add")
+const formMessage = document.querySelector("#form-message")
 const message = document.querySelector("#message")
 const table = document.querySelector("table")
 const tbody = document.querySelector("tbody")
 const ul = document.querySelector("ul")
 
 const affichage = () => {
-    people.forEach((p, index) => {
-        const li = document.createElement("li")
-        li.innerText = p
-        ul.append(li)
+    tbody.innerText = ""
+    ul.innerText = ""
+    if(people.length>0){
+        people.forEach((p, index) => {
+            
+            const li = document.createElement("li")
+            li.innerText = p
+            ul.append(li)
+    
+            const tr = document.createElement("tr")
+            const tdNom = document.createElement("td")
+            const tdPrenom = document.createElement("td")
+            const tdEmail = document.createElement("td")
+            const tdSupprimer = document.createElement("td")
+    
+            const firstname = p.split(" ")[0]
+            const lastname = p.split(" ")[1]
+    
+            tdNom.innerText = lastname
+            tdPrenom.innerText = firstname
 
-        const tr = document.createElement("tr")
-        const tdNom = document.createElement("td")
-        const tdPrenom = document.createElement("td")
-        const tdEmail = document.createElement("td")
-        const tdSupprimer = document.createElement("td")
-
-        const firstname = p.split(" ")[0]
-        const lastname = p.split(" ")[1]
-
-        tdNom.innerText = lastname
-        tdPrenom.innerText = firstname
-        tdEmail.innerText = `${firstname}.${lastname}@exemple.com`
-        tdSupprimer.innerHTML = `<a href="" id="${index}" class="btn-suppr">X</a>`
-
-        tbody.append(tr)
-        tr.append(tdNom, tdPrenom, tdEmail, tdSupprimer)
-    })  
+            const cleanedFirstname = firstname.normalize("NFD").replace(/[\u0300-\u036f]/g, "") 
+            const cleanedLastname = lastname.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+            
+            tdEmail.innerText = `${cleanedFirstname.toLowerCase()}.${cleanedLastname.toLowerCase()}@exemple.com`
+            tdSupprimer.innerHTML = `<a href="" id="${index}" class="btn-suppr">X</a>`
+    
+            tbody.append(tr)
+            tr.append(tdNom, tdPrenom, tdEmail, tdSupprimer)
+        })  
+        const btnSupprArray = document.querySelectorAll(".btn-suppr")
+        
+        btnSupprArray.forEach(b => {
+            b.addEventListener("click", (event) => {
+                event.preventDefault()
+                people.splice(b.id, 1)
+                affichage()
+            })
+        })
+    }
+    else{
+        table.innerText = ""
+        message.innerText = "La liste est vide !"
+    }
 }
 
-const btnSupprArray = document.querySelectorAll(".btn-suppr")
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
 
-btnSupprArray.forEach(b => {
-    b.addEventListener("click", (event) => {
-        event.preventDefault()
-        people.splice(b.id, 1)
+    const reg = /[a-zA-Z-]{2,}/
+    if(reg.test(inputFirstname.value.trim()) && reg.test(inputLastname.value.trim())){
+        const firstname = String(inputFirstname.value.trim()).charAt(0).toUpperCase() + " " + String(inputFirstname.value.trim())
+        const lastname = String(inputLastname.value.trim()).charAt(0).toUpperCase() + " " + String(inputLastname.value.trim())
+        people.push(firstname + ' ' + lastname)
         affichage()
-    })
+    }
 })
 
 affichage()
