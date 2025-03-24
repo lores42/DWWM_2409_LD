854c78d9bd5dec9199a99ed42f0bdb8c3f61674d
lores42 <148881509+lores42@users.noreply.github.com>
2025-03-24 15:57:18 +0100

    pays_js

---
+++ 02-WEBFRONT/JSTAB/gestionPays/assets/Pays.js
@@ -0,0 +1,21 @@
+export class Pays {
+
+    constructor(codePays, nomPays) {
+
+        if(!codePays || !nomPays) {
+            throw new Error("Vous devez renseigner les deux champs !");
+        }
+        else {
+            if(!/^[a-zA-Z]{2}$/.test(codePays)) {
+                throw new Error("Le code pays doit contenir exactement deux lettres non accentuées !");    
+            }
+    
+            if(!/^[a-zA-Z]{4,}$/.test(nomPays)) {
+                throw new Error("Le nom du pays doit contenir au moins quatre lettres non accentuées !");    
+            }
+        }
+
+        this.country_code = codePays
+        this.country_name = nomPays
+    }
+}
